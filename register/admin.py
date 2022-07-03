e051800b2e53a114574b925506d1e708e9adaf6d
Deep-Ace <hello.world.deepace@gmail.com>
2022-07-03 21:39:31 +0545

    Change Img

--- register/admin.py
+++ register/admin.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
-from .models import AddBook
+from register.models import AddBook
 from django.utils.html import format_html
 
 # Register your models here.
 
-class AddBookAdmin(admin.ModelAdmin):
+class BookAdmin(admin.ModelAdmin):
     def thumbnail(self, object):
         return format_html('<img src="{}" width="40" style="border-radius: 50px;" />'.format(object.b_pic.url))
 
-    thumbnail.short_description = 'Car Image'
-    list_display = ('id','thumbnail','b_name')
-    list_display_links = ('id', 'thumbnail', 'car_title')
-    
-admin.site.register(AddBook, AddBookAdmin)
+    thumbnail.short_description = 'Book Image'
+    list_display = ('book_id','thumbnail','b_name')
+    list_display_links = ('book_id', 'thumbnail', 'b_name')
+
+admin.site.register(AddBook, BookAdmin)
